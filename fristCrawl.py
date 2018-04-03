32fb806355b73a44f5fd486e7f302ce48de32408
public-long <1106875923@qq.com>
2018-04-03 20:10:01 +0800

    对fristCrawl.py增加了缓存

--- fristCrawl.py
+++ fristCrawl.py
@@ -9,6 +9,66 @@ import robotparser
 import Queue
 import csv
 import lxml.html
+import socket
+
+
+DEFAULT_AGENT = 'wswp'
+DEFAULT_DELAY = 5
+DEFAULT_RETRIES = 1
+DEFAULT_TIMEOUT = 60
+
+class Downloader:
+	def __init__(self, delay=DEFAULT_DELAY, user_agent=DEFAULT_AGENT, proxies=None, num_retries=DEFAULT_RETRIES, timeout=DEFAULT_TIMEOUT, opener=None, cache=None):
+		socket.setdefaulttimeout(timeout)
+		self.throttle = Throttle(delay)
+		self.user_agent = user_agent
+		self.proxies = proxies
+		self.num_retries = num_retries
+		self.opener = opener
+		self.cache = cache
+
+
+	def __call__(self, url):
+		result = None
+		if self.cache:
+			try:
+				result = self.cache[url]
+			except KeyError:
+				pass
+			else:
+				if self.num_retrie > 0 and 500 <= result['code'] < 600:
+					result = None
+		if result is None:
+			self.throttle.wait(url)
+			proxy = random.choice(self.proxies) if self.proxies else None
+			headers = {'User-agent': self.user_agent}
+			result = self.download(url, headers, proxy, self.num_retries)
+			if self.cache:
+				self.cache[url] = result
+		return result['html']
+
+	def download(self, url, headers, proxy, num_retrie, data = None):
+		print 'Download:', url
+		request = urllib2.Request(url, data, headers or {})
+		opener = self.opener or urllib2.build_opener()
+		if proxy:
+			proxy_params = {urlparse.urlparse(url).scheme: proxy}
+			opener.add_handler(urllib2.ProxyHandler(proxy_params))
+		try:
+			response = opener.open(request)
+			html = response.read()
+			code = response.code
+		except Exception as e:
+			print 'Download error', str(e)
+			html = ''
+			if hasattr(e, 'code'):
+				code = e.code
+				if num_retrie > 0 and 500 <= code < 600:
+					return _get(url, headers, proxy, num_retrie-1,data)
+			else:
+				code = None
+		return {'html': html, 'code': code}
+
 
 FIELDS = ('area', 'population', 'iso', 'country', 'capital', 'continent', 'tld', 'currency_code', 'currency_name', 'phone', 'postal_code_format', 'postal_code_regex', 'languages', 'neighbours')
 
@@ -58,7 +118,7 @@ def crawl_sitemap(url):
 
 #crawl_sitemap('http://example.webscraping.com/sitemap.xml')
 
-def link_crawler(seed_url, link_regex, delay=2, max_depth=2, scrape_callback=None):
+def link_crawler(seed_url, link_regex, delay=2, max_depth=2, scrape_callback=None, cache=None):
 	max_depth = 2
 	crawl_queue = [seed_url]
 	seen = {seed_url: 0}
@@ -70,7 +130,10 @@ def link_crawler(seed_url, link_regex, delay=2, max_depth=2, scrape_callback=Non
 	while crawl_queue:
 		url = crawl_queue.pop()
 		throttle.wait(url)
-		html = download(url)
+		#html = download(url)
+		D = Downloader(delay=delay, cache=cache)
+		html = D(url)
+
 
 		depth = seen[url]
 		links = []
@@ -151,6 +214,5 @@ class ScrapeCallback:
 
 #http://example.webscraping.com/places/default/index/25
 #http://example.webscraping.com/places/default/view/Aland-Islands-2
-
-
-link_crawler('http://example.webscraping.com', '/places/default/(index|view)', scrape_callback=ScrapeCallback())
+if __name__ == "__main__":
+	link_crawler('http://example.webscraping.com', '/places/default/(index|view)', scrape_callback=ScrapeCallback())
