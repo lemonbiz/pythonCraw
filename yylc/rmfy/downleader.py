0e4b354af089abd1d6a252993c967a97e06ac04b
public-long <1106875923@qq.com>
2018-04-09 23:27:58 +0800

    用字典储存了各个省份的诉讼公告url、省份名称、省份url

---
+++ yylc/rmfy/downleader.py
@@ -0,0 +1,196 @@
+#create by yangyinglong at 20180409 for yylc crawl ymfy
+# -*- coding: utf-8 -*- 
+
+import urllib2
+import builtwith
+import random
+import whois
+import re
+import urlparse
+from datetime import datetime, timedelta
+import time
+import robotparser
+import Queue
+import csv
+import lxml.html
+import socket
+import os
+import pickle
+from pymongo import MongoClient
+import threading
+import sys
+
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+DEFAULT_AGENT = 'wswp'
+DEFAULT_DELAY = 5
+DEFAULT_RETRIES = 1
+DEFAULT_TIMEOUT = 60
+SLEEP_TIME = 1
+
+results = {}
+
+class DiskCache(object):
+	"""docstring for DiskCache"""
+	def __init__(self, cache_dir='cache',expires=timedelta(days=30)):
+		self.cache_dir = cache_dir
+		self.expires = expires
+
+
+	def url_to_path(self, url):
+		components = urlparse.urlsplit(url)
+		path = components.path
+		if not path:
+			path = '/index.html'
+		elif path.endswith('/'):
+			path = path + '/index.html'
+		filename = components.netloc + path + components.query
+		filename = re.sub('[^/0-9a-zA-Z\-.,;_]', '_', filename)
+		filename = '/'.join(segment[:255] for segment in filename.split('/'))
+		return os.path.join(self.cache_dir, filename)
+
+	def __getitem__(self, url):
+		path = self.url_to_path(url)
+		if os.path.exists(path):
+			with open(path, 'rb') as fp:
+				result, timestamp = pickle.load(fp)
+				if self.has_expired(timestamp):
+					raise KeyError(url + ' has expires')
+				return result
+		else:
+			raise KeyError(url + ' does not exist')
+
+	def __setitem__(self, url, result):
+		path = self.url_to_path(url)
+		folder = os.path.dirname(path)
+		timestamp = datetime.utcnow()
+		data = pickle.dumps((result, timestamp))
+		if not os.path.exists(folder):
+			os.makedirs(folder)
+		with open(path, 'wb') as fp:
+			fp.write(data)
+
+	def has_expired(self, timestamp):
+		return datetime.utcnow() > timestamp + self.expires
+
+
+class Throttle:
+	def __init__(self, delay):
+		self.delay = delay
+		self.domains = {}
+
+	def wait(self, url):
+		domain = urlparse.urlparse(url).netloc
+		last_accessed = self.domains.get(domain)
+
+		if self.delay > 0 and last_accessed is not None:
+			sleep_secs = self.delay - (datetime.now() - 
+				last_accessed).seconds
+			if sleep_secs > 0:
+				time.sleep(sleep_secs)
+		self.domains[domain] = datetime.now()
+
+
+class Downloader:
+	def __init__(self, delay=DEFAULT_DELAY, user_agent=DEFAULT_AGENT, proxies=None, num_retries=DEFAULT_RETRIES, timeout=DEFAULT_TIMEOUT, opener=None, cache=None):
+		socket.setdefaulttimeout(timeout)
+		self.throttle = Throttle(delay)
+		self.user_agent = user_agent
+		self.proxies = proxies
+		self.num_retries = num_retries
+		self.opener = opener
+		self.cache = cache
+
+
+	def __call__(self, url):
+		result = None
+		if self.cache:
+			try:
+				result = self.cache[url]
+			except KeyError:
+				pass
+			else:
+				if self.num_retries > 0 and 500 <= result['code'] < 600:
+					result = None
+		if result is None:
+			self.throttle.wait(url)
+			proxy = random.choice(self.proxies) if self.proxies else None
+			headers = {'User-agent': self.user_agent}
+			result = self.download(url, headers, proxy, self.num_retries)
+			if self.cache:
+				self.cache[url] = result
+		return result['html']
+
+	def download(self, url, headers, proxy, num_retrie, data = None):
+		print 'Download:', url
+		request = urllib2.Request(url, data, headers or {})
+		opener = self.opener or urllib2.build_opener()
+		if proxy:
+			proxy_params = {urlparse.urlparse(url).scheme: proxy}
+			opener.add_handler(urllib2.ProxyHandler(proxy_params))
+		try:
+			response = opener.open(request)
+			html = response.read()
+			code = response.code
+		except Exception as e:
+			print 'Download error', str(e)
+			html = ''
+			if hasattr(e, 'code'):
+				code = e.code
+				if num_retrie > 0 and 500 <= code < 600:
+					return _get(url, headers, proxy, num_retrie-1,data)
+			else:
+				code = None
+		return {'html': html, 'code': code}
+
+download = Downloader(cache=DiskCache())
+
+def get_links(html):
+	#<a href="/places/default/index/11">
+	webpage_regex = re.compile(r'<td( colspan="2")?><a( target="_blank")? href="(.*?)"', re.IGNORECASE)
+	urls = webpage_regex.findall(html)
+	urls = [url[2] for url in urls]
+	tree = lxml.html.fromstring(html)
+	provinces = []
+	tree = str(tree.cssselect('table.region')[0].text_content())
+	provinces = tree.split(' ')
+	pro = []
+	with open("provinces.txt", 'wb') as fp:
+		for province in provinces:
+			if province != '\r\n' and province != '' and province != '\r\n\r\n':
+				#print province
+				province = province.replace('\r\n','')
+				pro.append(province)
+				fp.write(province+'\n')
+	#print urls
+	hand = re.compile(r'http')
+	urlhand = 'http://www.rmfysszc.gov.cn'
+	with open('results.txt', 'wb') as fp:
+		for i in range(35):
+			if not re.match(hand ,urls[i]):
+				urls[i] = urlhand + urls[i]
+			results[i+1] = {'provinceName':pro[i], 'provinceUrl':urls[i], 'noticeUrl':None, 'urlList':{}}
+			fp.write(pro[i]+':'+urls[i]+'\n')
+
+	return urls, pro, results
+
+def getNoticeUrl(url=None):
+	html = download(url)
+	webpage_regex = re.compile(r'<area shape="rect" coords="184,5,311,29" href="(.*?)"', re.IGNORECASE)
+	r = webpage_regex.findall(html)[0]
+	print r
+	return r
+
+def main():
+	html = download('http://www.rmfysszc.gov.cn')
+	urls, provinces, _ = get_links(html)
+	print len(urls), len(provinces), len(results)
+	for i in range(32):
+		url = results[i+1]['provinceUrl']
+		results[i+1]['noticeUrl'] = getNoticeUrl(url)
+	print results
+
+
+if __name__ == "__main__":
+	main()
